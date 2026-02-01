b454f32ec8ddf9c495139f37976c1666094d4288
GayathriS-gayu <gayathri.ps2007@gmail.com>
2026-02-01 21:57:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode }) 
+=> {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/Portfolio/',
